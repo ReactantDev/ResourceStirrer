cd5839aca4dfcb7fb0733b02076800bbc714491c
Setako <zygdtako@outlook.com>
2019-12-24 12:15:07 +0800

    [Change] ItemResourcesTable allow grouping

--- src/main/kotlin/dev/reactant/resourcestirrer/annotation/ItemResourcesTable.kt
+++ src/main/kotlin/dev/reactant/resourcestirrer/annotation/ItemResourcesTable.kt
@@ -1,7 +1,57 @@
 package dev.reactant.resourcestirrer.annotation
 
+import dev.reactant.resourcestirrer.itemresource.ItemResource
 import dev.reactant.resourcestirrer.resourceloader.ClassLoaderResourceLoader
+import kotlin.reflect.KTypeProjection
+import kotlin.reflect.KVariance
+import kotlin.reflect.full.createType
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.full.isSubtypeOf
+import kotlin.reflect.jvm.jvmErasure
 
-abstract class ItemResourcesTable(val identifierPrefix: String) {
-    val resourceLoader = ClassLoaderResourceLoader(this.javaClass.classLoader)
+abstract class ItemResourcesTable(
+        val identifierPrefix: String,
+        private val _resourceLoader: ClassLoaderResourceLoader? = null
+) : Iterable<ItemResource> {
+    val resourceLoader get() = _resourceLoader ?: ClassLoaderResourceLoader(this.javaClass.classLoader)
+
+    class TableHeader(val identifierPrefix: String, val resourceLoader: ClassLoaderResourceLoader?)
+
+    open val tableHeader: TableHeader get() = TableHeader(identifierPrefix, resourceLoader)
+
+    abstract class ItemResourcesGroup(_tableHeader: TableHeader, _identifierPrefix: String? = "")
+        : ItemResourcesTable(_tableHeader.identifierPrefix + _identifierPrefix, _tableHeader.resourceLoader) {
+        override val tableHeader: TableHeader get() = TableHeader(identifierPrefix, resourceLoader)
+    }
+
+
+    /**
+     * Explore the fields of the table by reflection
+     */
+    override fun iterator(): Iterator<ItemResource> {
+        val itemResourceType = ItemResource::class.createType()
+        val multipleItemResourceType = Iterable::class.createType(listOf(KTypeProjection(KVariance.OUT, itemResourceType)))
+
+        val multipleItemResource = this::class.declaredMemberProperties
+                .filter { it.returnType.isSubtypeOf(multipleItemResourceType) }
+                .map { it.getter.call(this) as Iterable<ItemResource> }
+                .flatten()
+
+        val singleItemResource = this::class.declaredMemberProperties
+                .filter { it.returnType.isSubtypeOf(itemResourceType) }
+                .map { it.getter.call(this) as ItemResource }
+
+
+        val objectMultipleItemResource = this::class.nestedClasses
+                .filter { it.isSubclassOf(multipleItemResourceType.jvmErasure) }
+                .map { it.objectInstance as Iterable<ItemResource> }
+                .flatten()
+
+        val objectSingleItemResource = this::class.nestedClasses
+                .filter { it.isSubclassOf(itemResourceType.jvmErasure) }
+                .map { it.objectInstance as ItemResource }
+
+        return multipleItemResource.union(singleItemResource).union(objectMultipleItemResource).union(objectSingleItemResource).iterator()
+    }
 }
--- src/main/kotlin/dev/reactant/resourcestirrer/collector/ItemResourcesTableService.kt
+++ src/main/kotlin/dev/reactant/resourcestirrer/collector/ItemResourcesTableService.kt
@@ -2,34 +2,16 @@ package dev.reactant.resourcestirrer.collector
 
 import dev.reactant.reactant.core.component.Component
 import dev.reactant.resourcestirrer.annotation.ItemResourcesTable
-import dev.reactant.resourcestirrer.itemresource.ItemResource
-import kotlin.reflect.KTypeProjection
-import kotlin.reflect.KVariance
-import kotlin.reflect.full.createType
-import kotlin.reflect.full.declaredMemberProperties
-import kotlin.reflect.full.isSubtypeOf
 
 /**
  * A service that use to explore the variables of object which extended ItemResourcesTable, and register the item resources
  */
 @Component
 class ItemResourcesTableService(
-        val itemResourceManagingService: ItemResourceManagingService
+        private val itemResourceManagingService: ItemResourceManagingService
 ) {
-
     fun addTable(table: ItemResourcesTable) {
-        val itemResourceType = ItemResource::class.createType()
-        val multipleItemResourceType = Iterable::class.createType(listOf(KTypeProjection(KVariance.OUT, itemResourceType)))
-        val multipleItemResource = table::class.declaredMemberProperties
-                .filter { it.returnType.isSubtypeOf(multipleItemResourceType) }
-                .map { it.getter.call(table) as Iterable<ItemResource> }
-                .flatten()
-        val singleItemResource = table::class.declaredMemberProperties
-                .filter { it.returnType.isSubtypeOf(itemResourceType) }
-                .map { it.getter.call(table) as ItemResource }
-
-        multipleItemResource.union(singleItemResource)
-                .let { itemResourceManagingService.addItem(*it.toTypedArray()) }
+        table.let { itemResourceManagingService.addItem(*it.toSet().toTypedArray()) }
     }
 }
 
