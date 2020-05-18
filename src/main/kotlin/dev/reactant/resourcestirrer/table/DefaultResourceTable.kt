f495748719786cec7e4f1ac3d42109c76233cfa1
Setako <zygdtako@outlook.com>
2020-05-18 14:49:33 +0800

    [Fix] Revert plugin yml and remove blank texture

--- src/main/kotlin/dev/reactant/resourcestirrer/table/DefaultResourceTable.kt
+++
@@ -1,9 +0,0 @@
-package dev.reactant.resourcestirrer.table
-
-import dev.reactant.resourcestirrer.resourcetype.item.byClassLoader
-import org.bukkit.Material
-
-@ResourcesTable
-object DefaultResourceTable : ItemResourcesTable("default") {
-    val BLANK = byClassLoader("textures/blank", "blank", Material.WOODEN_AXE)
-}
